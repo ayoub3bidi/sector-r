a0afe0968b7a395852ccf38fd9d2d7b7b6883b53
ayoub3bidi <mrayoubabidi@gmail.com>
2021-05-14 16:04:09 +0100

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDh0vPQ4p9Z0DFDqx9S8zVc0vV5EPVgKLM",
+    authDomain: "sector-r-database.firebaseapp.com",
+    projectId: "sector-r-database",
+    storageBucket: "sector-r-database.appspot.com",
+    messagingSenderId: "90155331711",
+    appId: "1:90155331711:web:155c403c54504ba8ae4f4a"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFireStore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFireStore, timestamp }
