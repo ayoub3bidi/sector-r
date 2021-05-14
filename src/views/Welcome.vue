d4a892efbeeb67e814edc71eb039d9ccf6ef8f27
ayoub3bidi <mrayoubabidi@gmail.com>
2021-05-14 15:43:05 +0100

    initialising the project

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcome container">
+      <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .welcome {
+        text-align: center;
+        padding: 20px 0;
+    }
+</style>
