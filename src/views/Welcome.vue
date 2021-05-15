d401d4d6635ed807e9c23ab0bb502c341e19134c
ayoub3bidi <mrayoubabidi@gmail.com>
2021-05-15 13:32:05 +0100

    switching between signUp & logIn

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="welcome container">
       <p>Welcome to <span>Sector R</span> live chatroom</p>
-      <SignupForm />
-      <LoginForm />
+      <div v-if="showLogin">
+          <h2>Login</h2>
+          <LoginForm/>
+          <p>No account yet? <span @click="showLogin = false">Signup</span></p>
+      </div>
+      <div v-else>
+          <h2>Signup</h2>
+          <SignupForm/>
+          <p>Already have an account?<span @click="showLogin = true">Login</span></p>
+      </div>
   </div>
 </template>
 
@@ -11,7 +19,12 @@ import SignupForm from '../components/SignupForm.vue'
 import LoginForm from '../components/LoginForm.vue'
 
 export default {
-    components: { SignupForm, LoginForm }
+    components: { SignupForm, LoginForm },
+    setup() {
+        const showLogin = ref(true)
+
+        return { showLogin }
+    }
 }
 </script>
 
@@ -37,5 +50,13 @@ export default {
         outline: none;
         color: #999;
         margin: 10px auto;
+    }
+    .welcome span {
+        font-weight: bold;
+        text-decoration: underline;
+        cursor: pointer;
+    }
+    .welcome button {
+        margin: 20px auto;
     }
 </style>
