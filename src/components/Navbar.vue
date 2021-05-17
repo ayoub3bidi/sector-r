8d73adbdfec2ac30e891a4ae6b86a407da4b6201
ayoub3bidi <mrayoubabidi@gmail.com>
2021-05-17 14:43:11 +0100

    logging users out

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+      <div>
+          <p>Hey there.. disable name here</p>
+          <p class="email">Currently logged in as... email</p>
+      </div>
+      <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+import useLogout from '../composables/useLogout'
+
+export default {
+    setup() {
+        const { logout, error } = useLogout()
+
+        const handleClick = async () => {
+            await logout()
+            if (!error.value) {
+                console.log('user logged out')
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/chatroom.vue
+++ src/views/chatroom.vue
@@ -1,13 +1,32 @@
 <template>
-  <p>chatroom</p>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script>
-export default {
+import Navbar from '../components/Navbar'
 
+export default {
+  components: { Navbar }
 }
 </script>
 
 <style>
-
+ nav {
+   padding: 20px;
+   border-bottom: 1px solid #eee;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+ }
+ nav p {
+   margin: 2px auto;
+   font-size: 16px;
+   color: #444;
+ }
+ nav p.email {
+   font-size: 14px;
+   color: #999;
+ }
 </style>
