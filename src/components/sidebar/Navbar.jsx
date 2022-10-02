18351e0ae852310fa75c6806130a1b4f33ccf8c7
ayoub3bidi <mrayoubabidi@gmail.com>
2022-10-02 13:25:50 +0100

    ✨ feat: add search

--- src/components/sidebar/Navbar.jsx
+++ src/components/sidebar/Navbar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '../../firebase'
+import { AuthContext } from '../../context/AuthContext'
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext)
   return (
     <div className="navbar">
       <span className="logo">Sector R</span>
       <div className="user">
-        <img src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-2379004.jpg&fm=jpg" alt="" />
-        <span>Jackie</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
--- src/components/sidebar/Search.jsx
+++ src/components/sidebar/Search.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from '../../firebase'
 
 const Search = () => {
+  const [username, setUsername] = useState("")
+  const [user, setUser] = useState(null)
+  const [err, setErr] = useState(false)
+
+  const handleSearch = async () => {
+    const q = query(collection(db, "users"), where("displayName", "==", username))
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => { setUser(doc.data()) });
+    } catch (err) {
+      setErr(true)
+    }
+  }
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch()
+  }
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder='Find a user...' />
+        <input type="text" placeholder='Find a user...' onKeyDown={handleKey} onChange={e => setUsername(e.target.value)}/>
       </div>
-      <div className="userChat">
-        <img src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-2379004.jpg&fm=jpg" alt="" />
+      { err && <span>Something went wrong please try again.</span> }
+      { user && <div className="userChat">
+        <img src={user.photoURL} alt="" />
         <div className="userChatInfo">
-          <span>Jane</span>
+          <span>{user.displayName}</span>
         </div>
-      </div>
+      </div>}
     </div>
   )
 }
